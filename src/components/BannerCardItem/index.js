89099401c02a7d25f8ef0b7e6f407aafc0f6f049
kesava <chkesava54@gmail.com>
2023-08-05 17:29:44 +0000

    adding files

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,16 @@
+const BannerCardItem = props => {
+  const {sourceItem} = props
+  const {headerText, description, className} = sourceItem
+  return (
+    <li className={className}>
+      <div>
+        <h1 className="card-heading">{headerText}</h1>
+        <p className="card_description">{description}</p>
+        <button className="card-btn" type="button">
+          Show More
+        </button>
+      </div>
+    </li>
+  )
+}
+export default BannerCardItem
